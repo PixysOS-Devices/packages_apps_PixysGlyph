AOSPA/android_packages_apps_ParanoidGlyph

ParanoidGlyph: Add support for essential notifications
 * Also move FileUtils.writeLineFromSlug logic from AnimationManager to seaparate function

Change-Id: Ib5984ebc22e85284c306597d1861b018cf885383

src/co/aospa/glyph/Tiles/TorchTileService.java
@@ -25,6 +25,7 @@ import co.aospa.glyph.R;
 import co.aospa.glyph.Constants.Constants;
 import co.aospa.glyph.Manager.StatusManager;
 import co.aospa.glyph.Utils.FileUtils;
+import co.aospa.glyph.Utils.ResourceUtils;
 
 /** Quick settings tile: Glyph **/
 public class TorchTileService extends TileService {
@@ -58,5 +59,9 @@ public class TorchTileService extends TileService {
     private void setEnabled(boolean enabled) {
         StatusManager.setAllLedsActive(enabled);
         FileUtils.writeLineFromSlug("all", enabled ? Constants.getMaxBrightness() : 0);
+        if (StatusManager.isEssentialLedActive() && !enabled)
+            FileUtils.writeLineFromSlug(
+                ResourceUtils.getString("glyph_settings_notifs_essential_slug"),
+                Constants.getMaxBrightness( )/ 100 * 7);
     }
 }

PixysOS-Devices/packages_apps_PixysGlyph

 * NotificationListenerService does not need to be started or stopped manually in ServiceUtils because it's automatically started and stopped by the system
Signed-off-by: aswin7469 <aswinas@pixysos.com>

src/com/pixys/glyph/Tiles/TorchTileService.java
@@ -25,6 +25,7 @@ import com.pixys.glyph.R;
 import com.pixys.glyph.Constants.Constants;
 import com.pixys.glyph.Manager.StatusManager;
 import com.pixys.glyph.Utils.FileUtils;
+import com.pixys.glyph.Utils.ResourceUtils;
 
 /** Quick settings tile: Glyph **/
 public class TorchTileService extends TileService {
@@ -58,5 +59,9 @@ public class TorchTileService extends TileService {
     private void setEnabled(boolean enabled) {
         StatusManager.setAllLedsActive(enabled);
         FileUtils.writeLineFromSlug("all", enabled ? Constants.getMaxBrightness() : 0);
+        if (StatusManager.isEssentialLedActive() && !enabled)
+            FileUtils.writeLineFromSlug(
+                ResourceUtils.getString("glyph_settings_notifs_essential_slug"),
+                Constants.getMaxBrightness( )/ 100 * 7);
     }
 }