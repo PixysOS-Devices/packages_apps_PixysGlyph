fbdaec303474045406394bec402a7e232105f7e8
Abhay Singh Gill <abhaygill017@gmail.com>
2024-03-09 19:46:47 +0530

    Revert "ParanoidGlyph: Update wired charging animation"
    Reason:-
    Just my personal preference.
    
    This reverts commit 4d9ca383f4ab5d722d10900b05c01ed5b7c13dec.

--- src/com/pixys/glyph/Manager/AnimationManager.java
+++ src/com/pixys/glyph/Manager/AnimationManager.java
@@ -148,27 +148,27 @@ public final class AnimationManager {
 
             try {
                 for (int i = 0; i < batteryArray.length; i++) {
-                    if (checkInterruption("charging")) throw new InterruptedException();
-                    batteryArray[i] = Constants.getBrightness();
-                    if (batteryDot && i == 0) continue;
-                    updateLedFrame(batteryArray);
-                    Thread.sleep(15);
-                }
-                for (int i = batteryArray.length - 1; i > amount - 1; i--) {
-                    if (checkInterruption("charging")) throw new InterruptedException();
-                    batteryArray[i] = 0;
-                    updateLedFrame(batteryArray);
-                    Thread.sleep(5);
+                    if ( i <= amount - 1 && batteryLevel > 0) {
+                        if (checkInterruption("charging")) throw new InterruptedException();
+                        batteryArray[i] = Constants.getBrightness();
+                        if (batteryDot && i == 0) continue;
+		                updateLedFrame(batteryArray);
+                        Thread.sleep(15);
+                    }
                 }
                 long start = System.currentTimeMillis();
-                while (System.currentTimeMillis() - start <= 2000) {
+                while (System.currentTimeMillis() - start <= 1000) {
                     if (checkInterruption("charging")) throw new InterruptedException();
                 }
-                for (int i = amount - 1; i >= 0; i--) {
-                    if (checkInterruption("charging")) throw new InterruptedException();
-                    batteryArray[i] = 0;
-                    updateLedFrame(batteryArray);
-                    Thread.sleep(11);
+                for (int i = batteryArray.length - 1; i >= 0; i--) {
+                    if ( i <= amount - 1 && batteryLevel > 0) {
+                        if (checkInterruption("charging")) throw new InterruptedException();
+                        if (batteryArray[i] != 0) {
+                            batteryArray[i] = 0;
+                            updateLedFrame(batteryArray);
+                            Thread.sleep(15);
+                        }
+                    }
                 }
                 long start2 = System.currentTimeMillis();
                 while (System.currentTimeMillis() - start2 <= 730) {
