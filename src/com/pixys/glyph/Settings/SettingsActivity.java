554fdf0668b1234328a074043a2dc45f4c635562
aswin7469 <aswinas@pixysos.com>
2024-03-23 15:48:11 +0530

    PixysGlyph: Fix build for 14 QPR2
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- src/com/pixys/glyph/Settings/SettingsActivity.java
+++ src/com/pixys/glyph/Settings/SettingsActivity.java
@@ -22,7 +22,7 @@ import android.app.Fragment;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
+import com.android.settingslib.collapsingtoolbar.R;
 
 public class SettingsActivity extends CollapsingToolbarBaseActivity {
 
