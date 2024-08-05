AgBKartikey/packages_apps_Glyph

ParanoidGlyph: Update essential led brightness when brightness is changed
Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

src/co/aospa/glyph/Services/AutoBrightnessService.java
@@ -29,6 +29,8 @@ import android.util.Log;
 import java.util.List;
 
 import co.aospa.glyph.Constants.Constants;
+import co.aospa.glyph.Manager.AnimationManager;
+import co.aospa.glyph.Manager.StatusManager;
 import co.aospa.glyph.Utils.ResourceUtils;
 
 public class AutoBrightnessService extends Service {
@@ -105,6 +107,8 @@ public class AutoBrightnessService extends Service {
                     " | BrightnessLux: " + led_lux + " | BrightnessValue: " + brightnessValue);
                 }
                 Constants.setBrightness(brightnessValue);
+                if (StatusManager.isEssentialLedActive())
+                    AnimationManager.playEssential();
             }
         }
 

src/co/aospa/glyph/Utils/ServiceUtils.java
@@ -24,7 +24,9 @@ import android.os.UserHandle;
 import android.util.Log;
 
 import co.aospa.glyph.Constants.Constants;
+import co.aospa.glyph.Manager.AnimationManager;
 import co.aospa.glyph.Manager.SettingsManager;
+import co.aospa.glyph.Manager.StatusManager;
 import co.aospa.glyph.Services.AutoBrightnessService;
 import co.aospa.glyph.Services.CallReceiverService;
 import co.aospa.glyph.Services.ChargingService;
@@ -126,7 +128,11 @@ public final class ServiceUtils {
 
     public static void checkGlyphService() {
         if (SettingsManager.isGlyphEnabled()) {
-            Constants.setBrightness(SettingsManager.getGlyphBrightness());
+            if (SettingsManager.getGlyphBrightness() != Constants.getBrightness()) {
+                Constants.setBrightness(SettingsManager.getGlyphBrightness());
+                if (StatusManager.isEssentialLedActive())
+                    AnimationManager.playEssential();
+            }
             if (SettingsManager.isGlyphChargingEnabled()) {
                 startChargingService();
             } else {

PixysOS-Devices/packages_apps_PixysGlyph

src/com/pixys/glyph/Services/AutoBrightnessService.java
@@ -30,6 +30,8 @@ import java.util.List;
 
 import com.pixys.glyph.Constants.Constants;
 import com.pixys.glyph.Utils.ResourceUtils;
+import com.pixys.glyph.Manager.AnimationManager;
+import com.pixys.glyph.Manager.StatusManager;
 
 public class AutoBrightnessService extends Service {
 
@@ -105,6 +107,8 @@ public class AutoBrightnessService extends Service {
                     " | BrightnessLux: " + led_lux + " | BrightnessValue: " + brightnessValue);
                 }
                 Constants.setBrightness(brightnessValue);
+                if (StatusManager.isEssentialLedActive())
+                    AnimationManager.playEssential();
             }
         }
 

src/com/pixys/glyph/Utils/ServiceUtils.java
@@ -34,6 +34,8 @@ import com.pixys.glyph.Services.PowershareService;
 import com.pixys.glyph.Services.VolumeLevelService;
 import com.pixys.glyph.Services.FaceDownService;
 import com.pixys.glyph.Services.AutoBrightnessService;
+import com.pixys.glyph.Manager.AnimationManager;
+import com.pixys.glyph.Manager.StatusManager;
 
 public final class ServiceUtils {
 
@@ -140,8 +142,12 @@ public final class ServiceUtils {
 
     public static void checkGlyphService() {
         if (SettingsManager.isGlyphEnabled()) {
-            Constants.setBrightness(SettingsManager.getGlyphBrightness());
+            if (SettingsManager.getGlyphBrightness() != Constants.getBrightness()) {
+                Constants.setBrightness(SettingsManager.getGlyphBrightness());
                startFaceDownService();
+                if (StatusManager.isEssentialLedActive())
+                    AnimationManager.playEssential();
+            }
             if (SettingsManager.isGlyphChargingEnabled()) {
                 startChargingService();
             } else {